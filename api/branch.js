2538a9989941e9cb20c4af7500503b214d6e93c4
sudarsank18 <116832833+sudarsank18@users.noreply.github.com>
2023-02-28 16:37:52 +0530

    Update branch.js

--- api/branch.js
+++ api/branch.js
@@ -1,1 +1,33 @@
+import express from 'express'
+import {createClient} from '@supabase/supabase-js'
+import bodyparse from 'body-parser'
+const app = express()
 
+const supabase = createClient(
+    'https://fehuecmfzeppnrhlvxqx.supabase.com',
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlaHVlY21memVwcG5yaGx2eHF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc1NzkyNTcsImV4cCI6MTk5MzE1NTI1N30.pXimYKqrNJzKj9UxrqV37WudBE1s7S13_MFVaZU0yaM'
+);
+app.use(bodyparse.json())
+app.use(
+    bodyparse.urlencoded({
+        extended: true,
+    })
+)
+
+export default async function branch(req, res) {
+    // client.query(`Select * from bank_branch where LOWER(branch) like '%${req.query.q.toLowerCase()}%' or lower(address) like '%${req.query.q.toLowerCase()}%' or LOWER(city) like '%${req.query.q.toLowerCase()}%' or LOWER(district) like '%${req.query.q.toLowerCase()}%' or LOWER(states) like '%${req.query.q.toLowerCase()}%' or LOWER(bank_name) like '%${req.query.q.toLowerCase()}%' order by ifsc  limit ${req.query.limit} offset ${req.query.offset}`, (err, result)=>{
+    //     if(!err){
+    //         res.send(result.rows);
+    //     }
+        
+    // });
+    // client.end;
+    console.log("hi")
+        const {data, error} = await supabase
+            .from('Bank')
+            .select()
+            .or(`branch.ilike.%${req.query.q.toLowerCase()}%`)
+            .order('ifsc' , {descending:true})
+            .range(parseInt(req.query.offset),parseInt(req.query.offset)+parseInt(req.query.limit)-1)
+            res.send(data)
+}
